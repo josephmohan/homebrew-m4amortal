56491bc759f5912c2f84c2483c2470236a21b851
josephmohan <jfmohan@gmail.com>
2026-02-04 09:14:52 -0800

    Update to v0.0.2

--- m4amortal.rb
+++ m4amortal.rb
@@ -1,8 +1,8 @@
 class M4amortal < Formula
   desc "Convert .m4a audio files to .wav from the command line"
   homepage "https://github.com/josephmohan/m4amortal"
-  url "https://github.com/josephmohan/m4amortal/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "1a1fc6fec4175100445c2b405da9e7fe8feb9f17de00496d0a2db1624676352f"
+  url "https://github.com/josephmohan/m4amortal/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "9998fe24538edb31da24175e889a11b57e7e62f9effa98ffa24cd707675279e7"
 
   depends_on "ffmpeg"
 
